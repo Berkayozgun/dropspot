2fcca819cc2f2aca0f96b53723b9c73b1d7e2af0
Berkayozgun <berkayozgun001@gmail.com>
2025-11-11 00:09:56 +0300

    feat: Add backend drop integration test

--- dropspot-backend/jest.config.js
+++ dropspot-backend/jest.config.js
@@ -2,9 +2,12 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '@prisma/client': '<rootDir>/generated/prisma/client.ts',
+  },
   roots: ['<rootDir>/src'],
   testMatch: [
-    '<rootDir>/src/tests/**/*.test.ts',
+    '**/*.test.ts',
   ],
   transform: {
     '^.+\\.ts$': ['ts-jest', {
